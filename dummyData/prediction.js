a83d14be87a19016c14f6d3a86f884737ac02be6
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-08-13 17:28:20 +0530

    sample data collection

---
+++ dummyData/prediction.js
@@ -0,0 +1,20 @@
+module.exports.predictionData={
+    predictions:[
+        {
+            productInput:"product_data1",
+            prediction:0.78,
+            actual:0.8
+        },
+        {
+            productInput:"product_data2",
+            prediction:0.76,
+            actual: 0.89
+        },{
+
+            productInput:"product_data3",
+            prediction:99,
+            actual:0.78
+
+        }
+    ]
+}
